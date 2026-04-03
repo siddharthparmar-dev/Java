b214722366d3d204390f4777a6548e8a9fda7949
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-04-03 15:45:54 +0530

    Added lambda

--- 02ObjectOrientedProgramming/src/com/OOPs/Lambda.java
+++ 02ObjectOrientedProgramming/src/com/OOPs/Lambda.java
@@ -1,23 +1,31 @@
 package com.OOPs;
 @FunctionalInterface
-interface  Alien1{      // Functional Interface
+interface Alien{      // Functional Interface
     void disp();        // only Single Abstract Method
 }
-class planet implements Alien1{
+class planet implements Alien {
     public void disp(){
         System.out.println("Disp method");
     }
 }
 public class Lambda {
     public static void main(String[] args) {
-//      Alien1 alien = new Alien1() {     //anonymous inner class // Direct implementation of interface
-//          @Override
-//          public void disp() {
-//              System.out.println("Anonymous inner class");
-//          }
-//      };
-//      alien.disp();
+      Alien alien1 = new Alien() {     //anonymous inner class // Direct implementation of interface
+          @Override
+          public void disp() {
+              System.out.println("Anonymous inner class");
+          }
+      };
+      alien1.disp();
 
-        // ==========>>>>> La,bda Expression
+        // ==========>>>>> Lambda Expression <<<<<<============
+        Alien alien2  = () -> System.out.println("Hello from lambda 1");
+        alien2.disp();
+
+        Alien alien3 = () -> {
+            System.out.println("Hello from lambda 2");
+        };
+        alien3.disp();
+
     }
 }
