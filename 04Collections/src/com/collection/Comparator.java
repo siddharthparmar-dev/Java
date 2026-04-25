cbd80280cdcf7188f431f8c52e0e409356d8b7c1
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-04-25 21:36:21 +0530

    added Comparator

---
+++ 04Collections/src/com/collection/Comparator.java
@@ -0,0 +1,62 @@
+package com.collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Cricketer{
+    int age;
+    String name;
+    double average;
+    double strikeRate;
+
+    public Cricketer() {
+
+    }
+
+    public Cricketer(int age, String name, double average, double strikeRate) {
+        super();
+        this.age = age;
+        this.name = name;
+        this.average = average;
+        this.strikeRate = strikeRate;
+    }
+
+    @Override
+    public String toString() {
+        return "Cricketer{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", average=" + average +
+                ", strikeRate=" + strikeRate +
+                '}';
+    }
+}
+class Alpha2 implements java.util.Comparator<Cricketer> {
+
+    @Override
+    public int compare(Cricketer c1, Cricketer c2) {
+        return (int) ((int) c1.strikeRate - c2.strikeRate);
+    }
+}
+public class Comparator {
+    public static void main(String[] args) {
+        ArrayList<Cricketer> list = new ArrayList<>();
+        list.add(new Cricketer(15,"vaibhav suryavanshi",50.23,150));
+        list.add(new Cricketer(38,"virat kohli",50.43,140));
+        list.add(new Cricketer(22,"abhishek sharma",35.23,180));
+
+        System.out.println(list);
+        Alpha2 alpha2 = new Alpha2();
+
+        Collections.sort(list,alpha2);
+        System.out.println(list);
+        // sort but on what basis?
+        // earlier we have single data so we can easily sort but here we have multiple data
+        // here comes the concept of comparable and comparartor
+
+        // comparator ==> it has only one abstract method
+        // we can use/access comarator using 3 ways 1. implements 2. anonymous inner class 3. Lambda
+
+
+    }
+}
