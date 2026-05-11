a264a1a6d076a941a4aeab07ef0e4d02901c523c
Siddharth <siddharthparmar.dev@gmail.com>
2026-05-11 19:42:44 +0530

    added Enums3

--- 04Collections/src/com/Enums_Annotations/Enums2.java
+++ 04Collections/src/com/Enums_Annotations/Enums2.java
@@ -1,6 +1,14 @@
 package com.Enums_Annotations;
 enum Laptop{
     Dell(45000),MacBook(49000),HP(52000),Asus(55000),Lenovo(48000);
+    /*
+    Internally java create something like:
+    final static Laptop Dell = new Laptop(45000);
+
+    final static Laptop MacBook = new Laptop(49000);
+
+    final static Laptop HP = new Laptop(52000);
+     */
     int price;
 
     Laptop(int price){
---
+++ 04Collections/src/com/Enums_Annotations/Enums3.java
@@ -0,0 +1,22 @@
+package com.Enums_Annotations;
+enum Result{
+    PASS,FAIL,NR;
+
+    int marks;
+    Result(){
+        System.out.println("Constructor of enum");
+    }
+    public void setMarks(int marks){
+        this.marks = marks;
+    }
+    public int getMarks(){
+        return marks;
+    }
+}
+public class Enums3 {
+    public static void main(String[] args) {
+    Result result = Result.PASS;
+    result.setMarks(100);
+    System.out.println(result.getMarks());
+}
+}
