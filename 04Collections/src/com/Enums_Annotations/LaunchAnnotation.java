c05c145308c7dde01a7ad7fc6b31a919671cc156
Siddharth <siddharthparmar.dev@gmail.com>
2026-05-12 23:38:39 +0530

    Added Annotation

--- 04Collections/src/com/Enums_Annotations/LaunchAnnotation.java
+++ 04Collections/src/com/Enums_Annotations/LaunchAnnotation.java
@@ -11,7 +11,7 @@ class Animal{
     }
 }
 class Tiger extends Animal{
-    @Override
+    //@Override
     public void animalUsuallyHuntsAndEats() {
         System.out.println("Tiger USually Hunt and Eats");
     }
