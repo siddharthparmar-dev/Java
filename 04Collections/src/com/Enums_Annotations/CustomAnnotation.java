b1d3e271b074caf76eaa572bbd9678f424558bd0
Siddharth <siddharthparmar.dev@gmail.com>
2026-05-13 20:24:32 +0530

    Added Annotation Notes

--- 04Collections/src/com/Enums_Annotations/CustomAnnotation.java
+++ 04Collections/src/com/Enums_Annotations/CustomAnnotation.java
@@ -1,6 +1,32 @@
 package com.Enums_Annotations;
-interface ABD{
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME) //Tells java: How annotation Behaves
+@Target(ElementType.TYPE)          //Where it can be used
+@interface CricketPlayer{
+    String Country();
+
+    int innings() default 0;   //innings automatically = 0
+}
+@CricketPlayer(Country = "South Africa") //metadata attached to class Frameworks/tools can later read it.
+class ABD{
+    private int innings;
+
+    public ABD(){
+
+    }
+
+    public void setInnings(int innings){
+        this.innings = innings;
+    }
+
+    public int getInnings(){
+        return innings;
+    }
 }
 public class CustomAnnotation {
     public static void main(String[] args) {
--- 04Collections/src/com/Enums_Annotations/LaunchAnnotation2.java
+++ 04Collections/src/com/Enums_Annotations/LaunchAnnotation2.java
@@ -1,5 +1,5 @@
 package com.Enums_Annotations;
-interface 
+
 public class LaunchAnnotation2 {
     public static void main(String[] args) {
 
